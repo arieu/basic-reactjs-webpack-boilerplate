f31c0f1fe0dd9e22110f5578f391a1997ab1b67f
ivinteractive <francisco.r.ochoa@gmail.com>
2017-08-02 19:48:44 -0700

    Fixed Webpack loaders structure

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,29 +2,31 @@ var path = require('path');
 var webpack = require('webpack');
 
 const PUBLIC_DIR = path.join(__dirname, 'public');
-const BUILD_DIR = path.join(__dirname, 'public/dist')
+const BUILD_DIR = path.join(__dirname, 'public/dist');
 
 module.exports = {
-    entry: path.resolve(__dirname,'./src/index.js'),
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    module: {
-        loaders: [{
-            test: /\.js$/,
-            exclude: /(node_modules)/,
-            loaders: ['babel-loader']
-        }]
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public/dist')
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: BUILD_DIR,
-        hot: true,
-        open: true,
-        openPage: ''
-    }
-}
+  entry: path.resolve(__dirname,'./src/index.js'),
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: [
+        { loader: 'babel-loader' }
+      ]
+    }]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public/dist')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: BUILD_DIR,
+    hot: true,
+    open: true,
+    openPage: ''
+  }
+};
